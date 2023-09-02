b6f0bde66790e0fd173def46183bcc0745bca05a
mjkweon17 <mjkweon17@korea.ac.kr>
2023-09-03 06:23:33 +0900

    feat(api): Complete 'story/{scene_no}' API

--- app/routers/story.py
+++ app/routers/story.py
@@ -1,19 +1,19 @@
 from fastapi import APIRouter, Depends, status, HTTPException
 from database import get_db
 from sqlalchemy.orm import Session
-from models import Scene, Chat
+from models import Scene, Chat, Mission
 
 router = APIRouter(prefix="/story", tags=["story"])
 
-# chat, scene 정보 가져오기
+# chat, scene, mission 정보 가져오기
 @router.get("/", 
-            response_model=dict,
             status_code=status.HTTP_200_OK,
             response_description="Success")
 async def get_story_list(db: Session = Depends(get_db)):
     scene = db.query(Scene).all()
     chat = db.query(Chat).all()
-    return {"scene": scene, "chat": chat}
+    mission = db.query(Mission).all()
+    return {"scene": scene, "chat": chat, "mission": mission}
 
 # 특정 scene 정보 가져오기
 @router.get("/{scene_no}", response_model=dict)
@@ -30,19 +30,31 @@ async def get_scene_and_chats(scene_no: int, db: Session = Depends(get_db)):
 
     db.close()
 
+    # Mission 정보 가져오기
+    try:
+        mission = db.query(Mission).filter(Mission.scene_no == scene_no).first()
+    except:
+        mission = None
+
     # 결과를 딕셔너리로 구성
     result = {
         "scene_no": scene.scene_no,
         "scene_name": scene.scene_name,
         "background_image": scene.background_image,
         "location": scene.location,
-        "hint": scene.hint,
         "description": scene.description,
         "chat": []
     }
 
+    if mission is not None:
+        result["mission"] = {
+            "mission_no": mission.mission_no,
+            "mission_description": mission.mission_description,
+            "mission_hint": mission.mission_hint
+        }
+
     for chat in chats:
-        result["message"].append({
+        result["chat"].append({
             "chat_id": chat.chat_id,
             "person": chat.person,
             "profile": chat.profile,
